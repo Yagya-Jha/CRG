45b44ea37f2ae6c50bb510da031aad1bcf26b158
Yagya-Jha <74773203+Yagya-Jha@users.noreply.github.com>
2021-01-21 20:06:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var scorecard;
+
+var cars, car1, car2, car3, car4;
+
+var cae,caeref;
+
+var track, car1_img, car2_img, car3_img, car4_img;
+
+function preload(){
+  track = loadImage("../images/track.jpg");
+  car1_img = loadImage("../images/car1.png");
+  car2_img = loadImage("../images/car2.png");
+  car3_img = loadImage("../images/car3.png");
+  car4_img = loadImage("../images/car4.png");
+  ground = loadImage("../images/ground.png");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  caeref = database.ref('carsatend').on("value",read);
+  game = new Game();
+  game.getState();
+  game.start();
+  scorecard = new ScoreCard();
+  }
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+    console.log("cae = "+cae);
+    if(cae===4){
+      scorecard.display();
+    }
+  }
+}
+
+function read(data){
+  cae = data.val();
+}
